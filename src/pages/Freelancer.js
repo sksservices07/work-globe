c9cdcf0148492c2e555eb47a14b8c70f23c9a4fc
raghav-rama <hackerer528@gmail.com>
2023-01-19 18:40:18 +0530

    feat: add routes

--- src/pages/Freelancer.js
+++ src/pages/Freelancer.js
@@ -1,7 +1,9 @@
 import * as React from 'react'
 import FreelanceNavBar from '../components/FreelanceNavBar'
 
-import { Box, Paper, Typography } from '@mui/material'
+import { Box, Paper, Typography, ButtonBase } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
+
 // "
 // text-align: left;
 // align-items: flex-start;
@@ -9,10 +11,10 @@ import { Box, Paper, Typography } from '@mui/material'
 // "
 
 const Freelancer = () => {
+    const navigate = useNavigate();
     return (
         <Box sx={{
             flexGrow: 1,
-            backgroundColor: '#f5f5f5',
         }}>
             <FreelanceNavBar />
             <Box sx={{
@@ -23,40 +25,57 @@ const Freelancer = () => {
                 flexDirection: 'column',
                 gap: 1.2,
             }}>
-                <Paper elevation={3} sx={{ p: 2, width: '30vw', display: 'flex', alignItems: 'center'}}>
-                    <img src={require('../img/netflix-512.png')} alt="netflix" height={100} width={100} />
-                    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
-                        <Typography variant="h6" component="p" sx={{ textAlign: 'left', ml: 1, color: 'black' }}>
-                            Senior Web Developer
-                        </Typography>
-                        <Typography variant="subtitle2" component="p" sx={{ textAlign: 'left', ml: 1, color: 'black' }}>
-                            3 yrs experience
-                        </Typography>
-                    </Box>
-                     
-                </Paper>
-                <Paper elevation={3} sx={{ p: 2, width: '30vw', display: 'flex', alignItems: 'center' }}>
-                    <img src={require('../img/tata-logo-blue.png')} alt="tata" height={100} width={100} />
-                    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
-                        <Typography variant="h6" component="p" sx={{ textAlign: 'left', ml: 1, color: 'black' }}>
-                            Full Stack Web Developer
-                        </Typography>
-                        <Typography variant="subtitle2" component="p" sx={{ textAlign: 'left', ml: 1, color: 'black' }}>
-                            1 yrs experience
-                        </Typography>
-                    </Box>
-                </Paper>
-                <Paper elevation={3} sx={{ p: 2, width: '30vw', display: 'flex', alignItems: 'center' }}>
-                    <img src={require('../img/wipro-logo.png')} alt="wipro" height={100} width={100} />
-                    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
-                        <Typography variant="h6" component="p" sx={{ textAlign: 'left', ml: 1, color: 'black' }}>
-                            Full Stack Web Developer
-                        </Typography>
-                        <Typography variant="subtitle2" component="p" sx={{ textAlign: 'left', ml: 1, color: 'black' }}>
-                            13 yrs experience
-                        </Typography>
-                    </Box>
-                </Paper>
+                <ButtonBase
+                    onClick={() => {
+                        navigate('/jobpost', {state: {company: 'Netflix', role: 'Senior Web Developer', experience: '3 yrs experience'}});
+                    }}
+                >
+                    <Paper elevation={3} sx={{ p: 2, width: '30vw', display: 'flex', alignItems: 'center' }}>
+                        <img src={require('../img/netflix-512.png')} alt="netflix" height={100} width={100} />
+                        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+                            <Typography variant="h6" component="p" sx={{ textAlign: 'left', ml: 1, color: 'black' }}>
+                                Senior Web Developher
+                            </Typography>
+                            <Typography variant="subtitle2" component="p" sx={{ textAlign: 'left', ml: 1, color: 'black' }}>
+                                3 yrs experience
+                            </Typography>
+                        </Box>
+                    </Paper>
+                </ButtonBase>
+                <ButtonBase
+                    onClick={() => {
+                        navigate('/jobpost', {state: {company: 'Tata', role: 'Full Stack Web Developer', experience: '1 yrs experience'}});
+                    }}
+                >
+                    <Paper elevation={3} sx={{ p: 2, width: '30vw', display: 'flex', alignItems: 'center' }}>
+                        <img src={require('../img/tata-logo-blue.png')} alt="tata" height={100} width={100} />
+                        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+                            <Typography variant="h6" component="p" sx={{ textAlign: 'left', ml: 1, color: 'black' }}>
+                                Full Stack Web Developer
+                            </Typography>
+                            <Typography variant="subtitle2" component="p" sx={{ textAlign: 'left', ml: 1, color: 'black' }}>
+                                1 yrs experience
+                            </Typography>
+                        </Box>
+                    </Paper>
+                </ButtonBase>
+                <ButtonBase
+                    onClick={() => {
+                        navigate('/jobpost', {state: {company: 'Wipro', role: 'Full Stack Web Developer', experience: '13 yrs experience'}});
+                    }}
+                >
+                    <Paper elevation={3} sx={{ p: 2, width: '30vw', display: 'flex', alignItems: 'center' }}>
+                        <img src={require('../img/wipro-logo.png')} alt="wipro" height={100} width={100} />
+                        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+                            <Typography variant="h6" component="p" sx={{ textAlign: 'left', ml: 1, color: 'black' }}>
+                                Full Stack Web Developer
+                            </Typography>
+                            <Typography variant="subtitle2" component="p" sx={{ textAlign: 'left', ml: 1, color: 'black' }}>
+                                13 yrs experience
+                            </Typography>
+                        </Box>
+                    </Paper>
+                </ButtonBase>
             </Box>
         </Box>
     )

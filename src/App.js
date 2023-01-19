0e475ad2df65c7bedc84bb91e1bc51d46c97f489
raghav-rama <hackerer528@gmail.com>
2023-01-19 18:37:53 +0530

    feat: create JobPost route

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Employer from './pages/Employer';
 import Freelancer from './pages/Freelancer';
 import Landing from './pages/Landing';
 import Footer from './components/Footer';
+import JobPost from './pages/JobPost';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Route exact path="/" element={<Landing />} />
         <Route exact path="/freelancer" element={<Freelancer />} />
         <Route exact path="/employer" element={<Employer />} />
+        <Route exact path='/jobpost' element={<JobPost/> } />
       </Routes>
       <Footer />
     </div>

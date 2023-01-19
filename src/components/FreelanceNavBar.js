1f90f327d851f4d3e7f8dccb7e4726f59222eebe
raghav-rama <hackerer528@gmail.com>
2023-01-19 07:29:43 +0530

    feat: add navbar for freelancer page

---
+++ src/components/FreelanceNavBar.js
@@ -0,0 +1,48 @@
+import * as React from 'react';
+
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { AppBar, Button, Typography, Box, Toolbar } from '@mui/material'
+
+const theme = createTheme({
+    components: {
+        MuiAppBar: {
+            styleOverrides: {
+                root: {
+                    backgroundColor: '#474747',
+                }
+            }
+        },
+        MuiButton: {
+            styleOverrides: {
+                root: {
+                    backgroundColor: '#1fe47a',
+                    height: '50px',
+                    width: '200px',
+                    fontSize: '20px',
+                    '&:hover': {
+                        transform: 'scale(1.05)',
+                        backgroundColor: '#1fe47a',
+                    }
+                }
+            }
+        }
+    }
+});
+
+const FreelanceNavBar = () => {
+    return (
+        <ThemeProvider theme={theme}>
+            <Box component="nav" sx={{ flexGrow: 1 }}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <Typography variant="h5" component="p" sx={{ flexGrow: 1, textAlign: 'left', ml: 2 }}>
+                            My Posted Jobs
+                        </Typography>
+                        <Button variant="contained">Post Job</Button>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+        </ThemeProvider>
+    );
+}
+export default FreelanceNavBar;

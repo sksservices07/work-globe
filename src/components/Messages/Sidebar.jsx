48409f07896a70a73dd8d9e3234c11ea7b5c7180
prajnastra <coder.abhijit90@gmail.com>
2023-01-22 10:01:42 +0530

    add: sidebar in message page

---
+++ src/components/Messages/Sidebar.jsx
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+import { Tab, Typography, Box, Button, Tabs } from "@mui/material";
+import MessageIcon from "@mui/icons-material/Message";
+import ContactsIcon from "@mui/icons-material/Contacts";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`message-tabpanel-${index}`}
+      aria-labelledby={`message-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `message-tab-${index}`,
+    "aria-controls": `message-tabpanel-${index}`,
+  };
+}
+
+function Sidebar() {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box
+      sx={{
+        width: "20%",
+        height: "83vh",
+        borderRight: 1,
+        borderColor: "divider",
+      }}
+      position="relative"
+    >
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="message sidebar"
+        >
+          <Tab icon={<MessageIcon />} label="Conversations" {...a11yProps(0)} />
+          <Tab icon={<ContactsIcon />} label="Contacts" {...a11yProps(1)} />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}>
+        Conversations
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Contacts
+      </TabPanel>
+      <Button
+        variant="contained"
+        sx={{
+          position: "absolute",
+          bottom: "0",
+          left: "0",
+        }}
+        color="primary"
+        size="large"
+        fullWidth
+      >
+        New Conversation
+      </Button>
+    </Box>
+  );
+}
+
+export default Sidebar;
--- src/pages/Messages.jsx
+++ src/pages/Messages.jsx
@@ -4,21 +4,18 @@ import { Box, Grid } from "@mui/material";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 import Navbar from "../components/NavBar";
+import Sidebar from "../components/Messages/Sidebar";
 
 const theme = createTheme({
-  components: {
-    MuiButton: {
-      styleOverrides: {
-        root: {
-          backgroundColor: "#1fe47a",
-          "&:hover": {
-            transform: "scale(1.05)",
-            backgroundColor: "#1fe47a",
-          },
-        },
-      },
-    },
-  },
+  // components: {
+  //   MuiButton: {
+  //     styleOverrides: {
+  //       root: {
+  //         backgroundColor: "#1fe47a",
+  //       },
+  //     },
+  //   },
+  // },
 });
 
 function Messages() {
@@ -28,6 +25,7 @@ function Messages() {
       <ThemeProvider theme={theme}>
         <Box sx={{ flexGrow: 1, m: 2 }}>
           <Grid container spacing={2}>
+            <Sidebar />
           </Grid>
         </Box>
       </ThemeProvider>
@@ -35,4 +33,4 @@ function Messages() {
   );
 }
 
-export default Messages; 
+export default Messages;

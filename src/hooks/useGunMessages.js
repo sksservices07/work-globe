fa043f6f4e232de837ad604775f0aa258dc8a272
ARi2692 <arundhati.gunu@gmail.com>
2023-02-08 14:11:13 +0530

    Fix: Conversations

--- src/hooks/useGunMessages.js
+++ src/hooks/useGunMessages.js
@@ -1,27 +1,51 @@
 import { useState, useEffect } from "react";
-import Gun from "gun";
-import { API } from "../backend";
+// import Gun from "gun";
+// import { API } from "../backend";
+import useGunRef from "./useGunRef";
 
-const gun = Gun({
-  peers: [`${API}/gun`],
-});
+// const gun = Gun({
+//   peers: [`${API}/gun`],
+// });
 
 const useGunMessages = (chatId) => {
+  const { gunRef } = useGunRef();
+
   const [messages, setMessages] = useState([]);
 
-  useEffect(() => {
+  useEffect( () => {
+
+    const app = async () => {
+    // console.log(chatId);
     if (!chatId) return;
     setMessages([])
-    gun
-      .get(`messages/${chatId}`)
+    const dbRef = gunRef(`messages/${chatId}`);
+
+    // console.log(dbRef)
+    dbRef
       .map()
-      .once((gunData, key) => {
+      .once(function (gunData, key) {
+        // console.log(key, gunData)
+        
         const data = {
+          key: key,
           sender: gunData.sender,
           text: gunData.text,
         };
-        setMessages((prevMessages) => [...prevMessages, data]);
-      });
+
+        // console.log("gunData:", gunData);
+        setMessages((prevMessages) => {
+          const hasId = id => prevMessages.some(item => item.key === id);
+          if (hasId(key)) {
+            return [...prevMessages]
+          } else {
+            return [...prevMessages, data]
+          }
+        });
+
+      })};
+
+      app();
+
   }, [chatId]);
 
   return { messages };
